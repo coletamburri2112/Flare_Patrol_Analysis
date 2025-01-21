f7f69677f45247c807c3aa668db23ce07548f99d
Cole Tamburri <cole.tamburri@colorado.edu>
2025-01-21 13:56:20 -0700

    minor change to structure width

--- WORKING_SOURCE/structure_widths.py
+++ WORKING_SOURCE/structure_widths.py
@@ -25,23 +25,35 @@ from astropy.io import fits
 import skimage
 import scipy
 
+plt.close('all')
+
 path = '/Users/coletamburri/Desktop/VBI_Destretching/'
 folder_vbi = 'AXXJL' # 8 August X-class flare decay phase
-
+#folder_vbi = 'BDJKM' # 11 August M-class flare decay phase
+filename='postdestretch_histomatch_dataCube.fits'
 dir_list = os.listdir(path+folder_vbi)
 
 fullhalpha = fits.open(path+folder_vbi+'/'+dir_list[1])
-
-first_frame = fullhalpha[0].data[0,:,:]
+#fullhalpha = fits.open(path+folder_vbi+'/'+filename)
+first_frame = fullhalpha[0].data[2,:,:]
 
 fig,ax=plt.subplots(dpi=300,figsize=(10,10))
-ax.pcolormesh(first_frame[1000:1500,500:1000],cmap='grey')
+ax.pcolormesh(first_frame,cmap='grey')
 ax.set_aspect('equal')
 ax.invert_yaxis()
+
+plt.show()
+
+cc = plt.ginput(2,timeout = 40)
+ylo = int(cc[0][0])
+yhi = int(cc[1][0])
+xlo = int(cc[0][1])
+xhi = int(cc[1][1])
 
 xarr = np.arange(np.shape(first_frame)[0])
 yarr = np.arange(np.shape(first_frame)[1])
 
+#[1000:1500,500:1000]
 spatial_samp = 0.017 # for vbi red at 656nm
 arcsec_to_km = 725
 xarr_km = xarr*spatial_samp
@@ -50,14 +62,14 @@ yarr_km = yarr*spatial_samp
 XKM,YKM =np.meshgrid(xarr_km,yarr_km)
 
 fig,ax=plt.subplots()
-ax.pcolormesh(first_frame[1150:1250,700:900],cmap='grey')
+ax.pcolormesh(first_frame[xlo:xhi,ylo:yhi],cmap='grey')
 
 ax.invert_yaxis()
 ax.set_aspect('equal')
 plt.show()
 
 # extract
-framezoom = first_frame[1150:1250,700:900]
+framezoom = first_frame[xlo:xhi,ylo:yhi]
 
 # now use point and click to find the line you want...
 
@@ -96,18 +108,22 @@ end=int(bb[1][0])+1
 
 
 
-ynorm = [float(i)/max(profile[st:end])-1 for i in profile[st:end]]
+ynorm = [float(i)/max(profile[st:end])-1 for i in profile[st:end]] #if negative
+g_init = models.Gaussian1D(amplitude=-1., mean=0.55, stddev=.1) #if negative
 
-g_init = models.Gaussian1D(amplitude=-1., mean=0.55, stddev=.1)
+#ynorm = [float(i)/max(profile[st:end]) for i in profile[st:end]] # if positive
+#g_init = models.Gaussian1D(amplitude=1, mean=0.50, stddev=.1) #if positive
+
 fit_g = fitting.LevMarLSQFitter()
+
 gsmaller = fit_g(g_init, xdirection[st:end], ynorm)
 
 plt.figure(figsize=(8,5))
 plt.plot(xdirection[st:end], ynorm, 'ko')
-plt.plot(xdirection[st:end], gsmaller(xdirection[st:end]), label='Gaussian')
+plt.plot(xdirection[st:end], gsmaller(xdirection[st:end]), label=str(gsmaller.fwhm*arcsec_to_km))
 plt.xlabel('Position')
 plt.ylabel('Flux')
 plt.legend(loc=2)
 plt.show()
 
-print(gsmaller.stddev*arcsec_to_km)
+print(gsmaller.fwhm*arcsec_to_km)
