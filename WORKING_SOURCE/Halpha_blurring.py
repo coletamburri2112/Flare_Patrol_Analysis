minor change to blurring - correct variance

WORKING_SOURCE/Halpha_blurring.py
@@ -95,10 +95,18 @@ data = data1['brightening']
 # datahist = dataCubeTrackedhist
 
 halpha_samp = 0.017 #arcsec/pixel
-resolution_aia = .6 #arcsec spatial sampling of sdo/aia
-resolution_trace = .5 #spatial sampling of rhessi/trace
+resolution_aia_var = .6**2 #arcsec spatial sampling of sdo/aia
+resolution_trace_var = .5**2 #spatial sampling of rhessi/trace
+resolution_vbi_var = 0.017**2 #spatial sampling of DKIST/VBI in H-alpha filter
 
-pixels_psf_sig = round(resolution_trace/halpha_samp)
+# subtract the vbi resolution from that of the instrument in question to get 
+# the width of the PSF we want to convolve.  Covolution of two gaussians is a 
+# Gaussian of variance equal to the sum of the *variances* of the two Gaussians
+# , so assume observations are "already" convolved with th DKIST PSF
+
+# var = std**2
+# var_total**2 = var_dkist**2 + var_aia**2
+pixels_psf_sig = round((np.sqrt(resolution_aia_var-resolution_vbi_var))/halpha_samp)
 
 l=0
 fig,ax=plt.subplots(3,3,dpi=300);
@@ -112,6 +120,10 @@ xs2=np.arange(1800,3400,1)
 
 X2,Y2 = np.meshgrid(xs2,ys2)
 props = dict(edgecolor='black',facecolor='white', alpha=0.8,boxstyle='square,pad=0.4')
+
+convolved_bright= gaussian_filter(np.asarray(data[20]),pixels_psf_sig)
+
+fig,ax=plt.subplots(dpi=200);ax.imshow(convolved_bright,cmap='grey');fig.show()
 
 for i in np.arange(0,90,10):
     convolved= gaussian_filter(np.asarray(data[i]),pixels_psf_sig)
@@ -134,7 +146,7 @@ for i in np.arange(0,90,10):
 fig.subplots_adjust(wspace=.04, hspace=.04)
 fig.show()
 
-fig.savefig('/Users/coletamburri/Desktop/brightevole.png')
+fig.savefig('/Users/coletamburri/Desktop/brightevolve.png')
 
 
 

WORKING_SOURCE/structure_widths.py
@@ -25,14 +25,14 @@ def double_gaussian( x, c1, mu1, sigma1, c2, mu2, sigma2 ,m,b):
         
 # Switches
 gauss2 = 0 # double-gaussian models?
-save = 0 # save output arrays?
-directory = '/Users/coletamburri/Desktop/small_loop_frame47_pd/'
-time = '2024-08-08T20:15:41.666666'
+save = 1 # save output arrays?
+directory = '/Users/coletamburri/Desktop/small_loop_frame3_pre_destretch_separate_all/'
+time = '2024-08-08T20:12:45.666666'
 if os.path.isdir(directory) == 0:
     os.mkdir(directory)
 filenamesave = directory+'widths_errors.npz' # filename for output
-numareas = 1 # number of areas to look at
-numcuts = 1 # number of strands of interest per area
+numareas = 5 # number of areas to look at
+numcuts = 10 # number of strands of interest per area
 ampdir = 'neg'
 note = []
 
@@ -104,10 +104,10 @@ else:
 path = '/Users/coletamburri/Desktop/VBI_Destretching/'
 folder_vbi = 'AXXJL/' # 8 August X-class flare decay phase
 filename = 'AXXJLselection_predestretch.npz'
-array = np.load(path+folder_vbi+filename)['brightening']
+array = np.load(path+folder_vbi+filename)['first50'] #first50 or brightening
 
 #frame to work with
-frame = array[20,:,:]
+frame = array[3,:,:]
 
 # X and Y coordinates of frame
 xarr = np.arange(np.shape(frame)[0])
@@ -139,6 +139,7 @@ xhis=[]
     
 # Begin loops - first, define the number of areas to search through
 for i in range(0,2*numareas,2):
+    plt.close('all')
     
     # Extract coordinates
     ylo, yhi, xlo, xhi = int(cc[i][0]), int(cc[i+1][0]), int(cc[i][1]),\