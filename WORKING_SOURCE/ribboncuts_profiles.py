8a31c2da49910835e2b728882cefe4b7dc59147c
Cole Tamburri <cole.tamburri@colorado.edu>
2025-03-04 16:51:09 -0700

    ribboncuts, intensity cal

--- WORKING_SOURCE/ribboncuts_profiles.py
+++ WORKING_SOURCE/ribboncuts_profiles.py
@@ -9,8 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n_points = 14
-shiftx = -12
-shifty = -720
+shiftx = 0
+shifty = 0
 num = 30
 
 # for upper portion
@@ -18,31 +18,31 @@ num = 30
 #y_inds = np.linspace(1270+shifty,1414+shifty,n_points)
 
 # for middle portion
-#x_inds = np.linspace(212,237,n_points)
+#x_inds = np.linspace(217,237,n_points)
 #y_inds = np.full((1,n_points),1100+shifty)[0]
 
 # for bottom portion
-x_inds = np.linspace(190+shiftx,214+shiftx,n_points)
-y_inds = np.linspace(1414+shifty,1270+shifty,n_points)
+x_inds = np.linspace(200+shiftx,220+shiftx,n_points)
+y_inds = np.linspace(520+shifty,550+shifty,n_points)
 
 colors = plt.cm.turbo(np.linspace(0,1,n_points))
 
 fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,5))
 
 for i in range(len(x_inds)):
-    ax1.plot(new_dispersion_range,bkgd_subtract_flaretime[int(x_inds[i]),:,int(y_inds[i])],c=colors[i],
+    ax1.plot(new_dispersion_range,scaled_flare_time[int(x_inds[i]),:,int(y_inds[i])],c=colors[i],
             linewidth=3)
     
-ax1.set_xlim([396.7,397.1])
+#ax1.set_xlim([396.7,397.1])
 ax1.grid()
 ax1.set_xlabel('Wavelength [nm]')
 ax1.set_ylabel('Intensity [$erg\;s^{-1}\;sr^{-1}\;\AA^{-1}\;cm^{-2}$]')
-ax1.set_xticks([396.8,396.9,397.0,397.1])
+#ax1.set_xticks([396.8,396.9,397.0,397.1])
     
 
-ax2.pcolormesh(np.transpose(obs_avg_caii),cmap='sdoaia304',alpha=.6)
-ax2.set_xlim([180,238])
-ax2.set_ylim([500,1600])
+ax2.pcolormesh(np.transpose(hbeta_avg),cmap='ocean',alpha=.6)
+#ax2.set_xlim([183,241])
+#ax2.set_ylim([0,1600])
 ax2.scatter(x_inds,y_inds,20,c=colors,edgecolors='black')
 ax2.set_aspect(.15)
 
